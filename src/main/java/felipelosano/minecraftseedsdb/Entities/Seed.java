588c8bbf040663eb48132468a4a0d711ad2a93bb
Felipe Losano <felipelosano@Outlook.com>
2024-08-06 22:13:29 -0300

    SeedEntity initial commit

--- src/main/java/felipelosano/minecraftseedsdb/Entities/Seed.java
+++ src/main/java/felipelosano/minecraftseedsdb/Entities/Seed.java
@@ -1,19 +1,11 @@
 package felipelosano.minecraftseedsdb.Entities;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import java.util.Date;
 
 @Entity
-@Table
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
+@Table(name = "seeds_tb")
 public class Seed {
 
   @Id
@@ -25,4 +17,59 @@ public class Seed {
   private Date dateOfPost;
   @ManyToOne
   private User user;
+
+  public Seed() {
+  }
+
+  public Seed(String seed, String img, String version, Date dateOfPost, User user) {
+    this.seed = seed;
+    this.img = img;
+    this.version = version;
+    this.dateOfPost = dateOfPost;
+    this.user = user;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public String getSeed() {
+    return seed;
+  }
+
+  public void setSeed(String seed) {
+    this.seed = seed;
+  }
+
+  public String getImg() {
+    return img;
+  }
+
+  public void setImg(String img) {
+    this.img = img;
+  }
+
+  public String getVersion() {
+    return version;
+  }
+
+  public void setVersion(String version) {
+    this.version = version;
+  }
+
+  public Date getDateOfPost() {
+    return dateOfPost;
+  }
+
+  public void setDateOfPost(Date dateOfPost) {
+    this.dateOfPost = dateOfPost;
+  }
+
+  public User getUser() {
+    return user;
+  }
+
+  public void setUser(User user) {
+    this.user = user;
+  }
 }
